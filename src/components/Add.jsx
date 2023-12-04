45ed7907f23dd5490f95417a8f00e441b52dfd05
PrajwalVK7 <prajwalvk98@gmail.com>
2023-12-04 12:01:22 +0530

    started creating home page

---
+++ src/components/Add.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Add() {
+    return (
+        <>
+            <div className='d-flex align-item-center mt-4'>
+            <button className='btn text-white ' style={{fontSize:'30px'}} >Uplaod New Video<i class="fa-solid fa-cloud-arrow-up ms-3"></i></button>
+            </div>
+        </>
+    )
+}
+
+export default Add
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,31 @@
- function Home(){
-    return(
-       <div>
-       </div>
+import { Link } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
+import Add from "../components/Add";
+function Home() {
+   return (
+      <div>
+         <div className="container">
+            <Row>
+               <Col lg={6} md={6}>
+                  <div className="add-videos d-flex justify-content-center">
+                     <Add/>
+                     {/* //add a new component */}
+                  </div>
+               </Col>
+               <Col >
+                  <div className="watch-history d-flex justify-content-center">
+                     <Link to={'/history'} style={{ textDecoration: "none", fontSize: "30px", color: "white" }}> Watch History</Link>
+                  </div>
+               </Col>
+            </Row>
+            <div className="container d-flex justify-content-between align-items-center">
+               <h4>all videos</h4>
+            </div>
 
-    )
+
+         </div>
+      </div>
+
+   )
 }
 export default Home;
